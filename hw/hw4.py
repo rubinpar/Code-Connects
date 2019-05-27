39ffb1580e282ab0774d6683c05eae0ccf7d2ed8
Rubin Parker <50557313+rubinpar@users.noreply.github.com>
2019-05-27 19:24:36 -0400

    Update hw4.py

--- hw/hw4.py
+++ hw/hw4.py
@@ -1,13 +1,17 @@
+import datetime
+
 first_name = input("What is your first name?")
 last_name = input("What is your last name?")
 birth_year = int(input("What year were you born in?"))
 
+current_year = datetime.datetime.now().year
+age = current_year - birth_year
+print(current_year)
+
 print("Hello {}".format(first_name))
 print("Hello {}".format(last_name))
 print("You were born in {:08d}".format(birth_year))
-print("The current year is 2.019e+03")
-print("That means you are 18 years old")
-birth = 2001
-current = 2019
-magicpercentage = birth % current
-print("Your magic percentage is: ".format(magicpercentage))
+print("The current year is {:.3E}".format(current_year))
+print("That means you are ", age, "years old")
+magicpercentage = birth_year / current_year
+print("Your magic percentage is: {:.4f} ".format(magicpercentage))
