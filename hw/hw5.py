000a69a5a3fd17a62582da53c327f35edb223961
Marcus Hughes <hughes.jmb@gmail.com>
2019-05-27 18:13:00 -0600

    Create hw5.py

---
+++ hw/hw5.py
@@ -0,0 +1,8 @@
+# HW:
+# 1. Print the odd numbers between 0 and 40 using variables to store 40
+#      and some kind of loop
+#      (2 different ways, maybe using range, for loop, a while loop)
+# 2. Add your two favorite numbers and then print the remainder when you divide
+#    that sum by your age. For example, if your favorite numbers were 1 and 2
+#    and you were 2 years old, you would print the number 1.
+#    Remember to use varaibles.
