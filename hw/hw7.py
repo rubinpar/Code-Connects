3fa46c463b59aaf3e55673773e8b92d9f9f886ff
jmbhughes <hughes.jmb@gmail.com>
2019-06-02 15:35:34 -0600

    created homework 7 and corrected battleships

---
+++ hw/hw7.py
@@ -0,0 +1,36 @@
+# 1. write a function
+# 2520 is the smallest number that can be divided
+# by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly
+# divisible by all of the numbers from 1 to 20?
+
+# 2. write another function
+# Book Inventory
+# Write a function called printBook that will print out the details of a book in
+# a book stores inventory.
+# Input Format
+#
+# The input parameters to your function should be the book title, author, num-
+# ber of copies of the book, and price of the book.
+#
+# Constraints
+# All the print statements need to happen inside the function. You should use
+# the str() function to turn a number into a string.
+# Output Format
+# Print the book details in the format below. Also output the store revenue if
+# they sell all the books in the inventory.
+# Sample Input
+# printBook(”I Know Why the Caged Bird Sings”, ”Maya Angelou”, 100, 20)
+# Sample Output
+# Title: I Know Why the Caged Bird Sing
+# Author: Maya Angelou
+# Number of Copies: 100
+# Price per Copy: $20
+# Revenue: $2000
+#
+# Explanation
+# We print out out the title, author, number of copies, price per copy, and
+# revenue on separate lines. The revenue is the number of copies multiplied by
+# the price per copy, which is 2000.
+
+# 3. think about a project
