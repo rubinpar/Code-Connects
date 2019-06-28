90da3c8d6ec74bfe8235a5393d2dfb4051877dc5
Rubin Parker <rubinparker@Rubins-MacBook-Pro.local>
2019-06-27 22:03:08 -0400

    Worked on day8.py

---
+++ lessons/day8.py
@@ -0,0 +1,91 @@
+class Complex:
+    """
+    Represents a complex number, e.g. a+bi.
+
+    Supports:
+        str method
+        add method
+        subtract method
+        divide method
+        multiply method
+        magnitude method
+    """
+    def __init__(self, real, imaginary):
+        """ create complex number with the specified real and imaginary parts"""
+        self.real = real
+        self.imaginary = imaginary
+
+    def __str__(self):
+        """ format a string"""
+        return "{} + {}i".format(self.real, self.imaginary)
+
+    def __add__(self, other):
+        """ add self and other """
+        return Complex(self.real + other.real, other.imaginary + self.imaginary)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imaginary - other.imaginary)
+
+    def __mul__(self, other):
+        """
+        Multiply two complex numbers.
+            To multiply complex numbers, use distribution.
+            (a + bi)(c + di) = ac + adi + bci + bdi^2
+                             = ac + adi + bci - bd
+                             = (ac - bd) + (ad + bc)i
+        """
+        real = self.real * other.real - self.imaginary * other.imaginary
+        imaginary = self.real * other.imaginary + self.imaginary * other.real
+        return Complex(real, imaginary)
+
+    def __div__(self, other):
+        """
+        Divide two complex numbers.
+            To divide complex numbers , multiply by the conjugate.
+        """
+        temp = self.__mull__(Complex(other.real, -other.imaginary))
+        denom = other.real * other.real - other.imaginary * other.imaginary
+        return Complex(temp.real/denom , temp.imaginary/denom)
+
+num1 = Complex(2, 3)
+num2 = Complex(-2, -3)
+print(num1)
+print(num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print((2+3j) * (-2-3j))
+#(2x + 3y)(4x + 6y)
+
+
+class Customer:
+    """
+    Bank customers!
+    """
+    def __init__(self, num):
+        self.balance = num
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+Brian = Customer(-100)
+Stephanie = Customer(-250)
+Brian.withdraw(401)
+Brian.deposit(80)
+
+# Use a function to determine their current balances
+# Update their balances using other functions so that Brian ends up with $100
+# and Stephanie ends up with $250
+
+
+# Make a pet store!
+# Want animal objects... what are their states, their operations?
+# We also want a pet store that has a selection of animals for customers
+# to choose from. So what's its state? And what might a pet store have to do
+# as operations, e.g. get more pets, feed a pet
